bedf428ba3300e1f173775ca4a8dc6f9de827ca1
studenti <studenti@rosselli.loc>
2020-02-03 08:43:23 +0100

    Add project files.

---
+++ StruttureDati2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dati_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome del file sorgente: ");
+            string file = $"{ Console.ReadLine()}";
+            Console.WriteLine("Nome del file nuovo: ");
+            string file2 = $"{ Console.ReadLine()}";
+            if (File.Exists(file))
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(file))
+                    {
+                        using (StreamWriter w = new StreamWriter(file2))
+                        {
+                            string line;
+                            while ((line = r.ReadLine()) != null)
+                            {
+                                w.WriteLine(line);
+                            }
+                        }
+                    }
+                }
+                catch 
+                { 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Il file sorgente non esiste");
+            }
+        }
+    }
+}
